19bc1b7af9a0729bd48331a5fee33686b7150b24
Sergey Kudrin <ksvcsv@inbox.ru>
2025-03-11 13:59:33 +0300

    feat: task solved

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -72,8 +72,16 @@ function getDayName(date) {
  * Date('2024-02-13T00:00:00Z') => Date('2024-02-16T00:00:00Z')
  * Date('2024-02-16T00:00:00Z') => Date('2024-02-23T00:00:00Z')
  */
-function getNextFriday(/* date */) {
-  throw new Error('Not implemented');
+function getNextFriday(date) {
+  const newDate = new Date(date);
+  const todayDay = newDate.getDay();
+  const daysToFriday = (5 - todayDay + 7) % 7;
+  if (daysToFriday === 0) {
+    newDate.setDate(newDate.getDate() + 7);
+  } else {
+    newDate.setDate(newDate.getDate() + daysToFriday);
+  }
+  return newDate;
 }
 
 /**
