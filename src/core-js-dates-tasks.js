75642116e397ee08a0e8543f6ecedd0c2de5b4be
Sergey Kudrin <ksvcsv@inbox.ru>
2025-03-18 14:47:27 +0300

    feat: some tasks done

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -152,8 +152,17 @@ function isDateInPeriod(date, period) {
  * '1999-01-05T02:20:00.000Z' => '1/5/1999, 2:20:00 AM'
  * '2010-12-15T22:59:00.000Z' => '12/15/2010, 10:59:00 PM'
  */
-function formatDate(/* date */) {
-  throw new Error('Not implemented');
+function formatDate(date) {
+  const fixDate = new Date(date);
+  const format = fixDate.getUTCHours() >= 12 ? 'PM' : 'AM';
+  const hours = fixDate.getUTCHours() % 12 || 12;
+  const minutes = fixDate.getUTCMinutes().toString().padStart(2, '0');
+  const seconds = fixDate.getUTCSeconds().toString().padStart(2, '0');
+  const day = fixDate.getUTCDate();
+  const month = fixDate.getUTCMonth() + 1;
+  const year = fixDate.getUTCFullYear();
+  const string = `${month}/${day}/${year}, ${hours}:${minutes}:${seconds} ${format}`;
+  return string;
 }
 
 /**
@@ -168,8 +177,17 @@ function formatDate(/* date */) {
  * 12, 2023 => 10
  * 1, 2024 => 8
  */
-function getCountWeekendsInMonth(/* month, year */) {
-  throw new Error('Not implemented');
+function getCountWeekendsInMonth(month, year) {
+  const lastDayMonth = new Date(year, month, 0);
+  const firstDayMonth = new Date(year, month - 1, 1);
+  let weekendsInMonth = 0;
+  for (let i = firstDayMonth; i <= lastDayMonth; i.setDate(i.getDate() + 1)) {
+    const weekDays = i.getDay();
+    if (weekDays === 0 || weekDays === 6) {
+      weekendsInMonth += 1;
+    }
+  }
+  return weekendsInMonth;
 }
 
 /**
